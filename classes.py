ffc8ef5fcba04d9dfc4a5fb7249cae64f6f27dc9
OrNitorinc0 <43863009+OrNitorinc0@users.noreply.github.com>
2018-10-27 19:42:25 +0200

    Add files via upload
    Added map, locations, plot, mój kotek jest fajny.
    Locations now do something.

--- classes.py
+++ classes.py
@@ -1,43 +1,53 @@
 class Location:
-    def __init__(self, short_desc, long_desc):
+    def __init__(self, short_desc, long_desc, same_room = None):
         self.short_desc = short_desc
         self.long_desc = long_desc
+        self.same_room = same_room
 
 class World:
-    data = [
-        [ 1, 2, 3, 0, 0, 0 ],
-        [ 0, 0, 2, 1, 1, 2 ],
-        [ 0, 0, 0, 2, 0, 3 ],
-        [ 0, 0, 0, 3, 2, 1 ],
-        [ 0, 0, 0, 0, 0, 1 ],
-        [ 0, 0, 0, 0, 0, 'F' ],
-    ]
-
+    ##Opisy lokacji
     descriptions = {
-        1: Location("maly pokoj", "Jestes w malym pokoju."),
-        2: Location("duzy pokoj", "Jestes w duzym pokoju."),
-        3: Location("pokoj", "Jestes w pokoju."),
-        'F': Location("wyjscie", "sfasd"),
+        '1': Location("Małe, skromnie ozdobione drzwi prowadzące do korytarza.",
+                    "Jesteś w ciasnym korytarzu, oświetlonym tylko przez wątły płomień pochodni.",
+                    "Dalsza część korytarza."),
+        '1_1': Location("Małe, skromnie ozdobione drzwi prowadzące do korytarza.",
+                        "Aktywowałeś pułapkę!",
+                        "Dalsza część korytarza."),
+        '2': Location("Ogromne i ciężkie drzwi, na klamce widnieje kurz.",
+                    "Sala obiadowa. Wyposażona w wiele krzeseł i dłuuugi stół. Na stole widnieją resztki jedzenia.",
+                    "Dalsza część jadalni, w oddali widać taki sam podłużny stół."),
+        '2_1': Location("Ogromne drzwi. Chyba ktoś już tutaj był...",
+                      "Sala obiadowa. Na stole widnieją puste naczynia...\nKtoś tutaj miał zapewne niezłą ucztę.",
+                      "Dalsza część jadalni."),
+        '4': Location("Dziwne ciemne drzwi.",
+                      "Na piedestale na środku pokoju widać mały amulet."),
+        '4_1': Location("Czarne drzwi, coś strasznego kryje się w środku.",
+                        "Masz przeczucie, że za niedługo może się tutaj wydarzyć coś złego."),
+        '4_2': Location("W tych drzwiach jest coś co cię do nich przyciąga.",
+                        "To była pułapka!\nCzarny duch który przebywał w tym pokoju, ledwo pozostawił cię przy życiu."),
+        '5': Location("Drzwi na których widnieją ślady używania czarnej magii.",
+                      "Prawdopodobnie, był to pokój jakiegoś czarnoksiężnika.\nWidać ślady przywoływań duchów."),
+        'E': Location( "AttributeError: 'World' object has no attribute 'data[6][16]'",
+                        "Hej! Czy twój pierwszy odruch, gdy widzisz Buga to wejście do niego? A co tam...\nGratulacje!\nUdało ci się wyjść z labiryntu!")
     }
 
-    def __init__(self):
-        self.width = 6
-        self.height = 6
+    def __init__(self, mapa):
+        self.data = mapa
+        self.width = len(self.data[0])-1
+        self.height = len(self.data)-1
 
     def exist(self, x, y):
         if x < 0 or x >= self.width:
             return False
         if y < 0 or y >= self.height:
             return False
-        return not self.data[y][x] == 0
-
-    def isExit(self, x, y):
-        return self.data[y][x] == 'F'
+        return self.data[y][x] != 0
 
     def getAt(self, x, y):
-        return self.descriptions[self.data[y][x]]
+        return self.descriptions[str(self.data[y][x])]
 
 class Character:
-    def __init__(self, x, y):
+    def __init__(self, x, y, hp):
         self.x = x
         self.y = y
+        self.hp = hp
--- main.py
+++ main.py
@@ -1,76 +1,177 @@
 import os
-
+from random import randint
 from classes import *
 
-game_running = True
-world = World()
-hero = Character(0, 0)
+##Mapa
+mapp = [[1,2, 1, 0, 2, 4, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 4],
+        [0, 1, 1, 0, 0, 1, 1, 1, 4, 0, 1],
+        [1, 4, 0, 2, 0, 5, 4, 0, 1, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1],
+        [1, 2, 0, 4, 0, 1, 0, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0, 4, 0, 0,'E',0, 1],
+        [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+        [5, 0, 1, 5, 1, 0, 1, 1, 1, 1, 0]]
+
+##Różne przydatne zmienne i obiekty
+world = World(mapp)
+hero = Character(0, 0, 10)
+Ended = False
+EndCoordinates = [0, 0]
+wrongAction = False
+counterx = 0
+countery = 0
+
+
+for i in range(20):  ##Dodawanie losowych pułapek
+    x = randint(0, world.width)
+    y = randint(0, world.height)
+    if str(world.data[y][x]) == '1':
+        world.data[y][x] = '1_1'
+
+for i in world.data:
+    for j in i:
+        if j == 'E':
+            EndCoordinates = [counterx, countery]
+        counterx += 1
+    countery +=1
+    counterx = 0
 
 keys = {
-    "polnoc": "w",
-    "poludnie": "s",
-    "zachod": "a",
-    "wschod": "d",
+    'w': "Północ:",
+    's': "Południe:",
+    'a': "Zachód:",
+    'd': "Wschód:",
+    'q': "Zabrać amulet?",
+    'e': "Zjeść ze stołu?"
 }
 
-def clearScreen():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
+##Funkcje
+def clearScreen():              ##Wieloplatformowość
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def EndDirection():
+    String = ""
+    if hero.y > EndCoordinates[1] + 1:
+        String += "północny "
+    elif hero.y < EndCoordinates[1] - 1:
+        String += "południowy "
+    if hero.x > EndCoordinates[0] + 1:
+        String += "zachód."
+    elif hero.x < EndCoordinates[0] - 1:
+        String += "wschód."
+    if String == "południowy ":
+        String = "południe."
+    elif String == "północny ":
+        String = "północ."
+    return String
+
+##Następne funkcje są do wyświetlanie menu
 def getFullOptionName(option_name):
-    return f"[{keys[option_name]}] {option_name}"
+    return f"[{option_name}] {keys[option_name]}"
 
 def printChoice(x, y, option_name):
-    if world.exist(x, y):
+    if x == None and y == None:
+        print(getFullOptionName(option_name))
+    elif world.exist(x, y):
         loc = world.getAt(x, y)
-        print(f"{getFullOptionName(option_name)}: {loc.short_desc}")
+        if world.data[hero.y][hero.x] != world.data[y][x]:
+            print(getFullOptionName(option_name), loc.short_desc)
+        elif world.data[hero.y][hero.x] == world.data[y][x]:
+            print(getFullOptionName(option_name), loc.same_room)
 
 def printChoices():
-    printChoice(hero.x - 1, hero.y, "zachod")
-    printChoice(hero.x + 1, hero.y, "wschod")
-    printChoice(hero.x, hero.y + 1, "poludnie")
-    printChoice(hero.x, hero.y - 1, "polnoc")
+    print(f"Najwyraźniej, twoja lokalizacja to: {hero.x}, {hero.y}")
+    print("\nKompas wskazuje:", EndDirection())
+    print("Twoje zdrowie to:", hero.hp, '\n')
+    printChoice(hero.x - 1, hero.y, "a")
+    printChoice(hero.x + 1, hero.y, "d")
+    printChoice(hero.x, hero.y + 1, "s")
+    printChoice(hero.x, hero.y - 1, "w")
+    if str(world.data[hero.y][hero.x]) == '2':
+        printChoice(None, None, 'e')
+    elif str(world.data[hero.y][hero.x]) == '4':
+        printChoice(None, None, 'q')
 
 def printCurrentLocation():
     print(f"{world.getAt(hero.x, hero.y).long_desc}")
 
 def moveHero(direction_x, direction_y):
+    global wrongAction
     next_pos_x = hero.x + direction_x
     next_pos_y = hero.y + direction_y
     if world.exist(next_pos_x, next_pos_y):
         hero.x = next_pos_x
         hero.y = next_pos_y
+    else:
+        wrongAction = True
 
 def checkinput(player_choice):
+    player_choice.lower()
     direction_x = 0
     direction_y = 0
 
-    if player_choice == keys["polnoc"]:
+    if str(world.data[hero.y][hero.x]) == '1_1':  ##Zmienianie odwiedzonych pokoi
+        hero.hp -= 2
+        world.data[hero.y][hero.x] = '1'
+    elif str(world.data[hero.y][hero.x]) == '4_1':
+        world.data[hero.y][hero.x] = '4_2'
+    elif str(world.data[hero.y][hero.x]) == '4_2':
+        hero.hp = 4
+        world.data[hero.y][hero.x] = '5'
+
+    if player_choice == 'w':
         direction_y = -1
-    elif player_choice == keys["wschod"]:
+    elif player_choice == 'd':
         direction_x = 1
-    elif player_choice == keys["poludnie"]:
+    elif player_choice == 's':
         direction_y = 1
-    elif player_choice == keys["zachod"]:
+    elif player_choice == 'a':
         direction_x = -1
+
+    elif player_choice == 'q' and str(world.data[hero.y][hero.x]) == '4':
+        world.data[hero.y][hero.x] = '4_1'
+        hero.hp += 4
+
+    elif player_choice == 'e' and str(world.data[hero.y][hero.x]) == '2':
+        chance = randint(0,5)
+        if chance <= 1:
+            hero.hp += randint(1, 5)
+        else:
+            hero.hp -= randint(3, 7)
+        world.data[hero.y][hero.x] = '2_1'
 
     return direction_x, direction_y
 
-def gameOver():
-    global game_running 
-    game_running = False
-    print("Ukonczyles gre.")
-    print("Nacisnij dowolny klawisz, aby kontynuowac.")
-    input()
 
-def loop():
+##Początek gry
+print("Uciekając przed bandytami, postanowiłeś się ukryć w pewnej jaskinii.\nNiestety potknąłeś się wpadając do lochów.\nTwój magiczny kompas wskazuje wyjście.")
+input("Naciśnij Enter by zacząć grę!")
+
+##ja bym to nazwał main()
+def mainLoop():
+    global wrongAction
+    clearScreen()
+
+    if hero.hp <= 0:
+        print('Twój bohater zginął.\nPrzegrałeś!')
+        Ended = 1
+        return 0
+
+    if wrongAction:
+        print("Nie możesz tego zrobić!\n")
+        wrongAction = 0
+
     printCurrentLocation()
     printChoices()
     player_choice = input("Wybierz kierunek: ")
     dir_x, dir_y = checkinput(player_choice)
     moveHero(dir_x, dir_y)
-    if world.isExit(hero.x, hero.y):
-        gameOver()
+    if world.data[hero.y][hero.x] == 'E':
+        Ended = 1
+        input("Naciśnij enter by zakończyć przygodę!")
 
-while game_running:
-    clearScreen()
-    loop()
+#Tutaj toczy się gra
+while not Ended:
+    mainLoop()
